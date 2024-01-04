d41be686a8819db93110439975f1c37554891f9d
Tor Ivar Melling <tor.i.melling@ntnu.no>
2024-01-04 14:02:13 +0100

    Update

---
+++ 01-04-Configure_MGR.ps1
@@ -0,0 +1,13 @@
+# Installere Choco 
+# Hva er Choco: https://chocolatey.org/
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+choco upgrade chocolatey
+# Installere programvare med Choco
+choco install -y powershell-core
+choco install -y git.install
+choco install -y vscode
+#choco install -y sysinternals
+
+# Konfigurer Git
+git config --global user.name "NAVN"
+git config --global user.email "EPOST@EPOST.EPOST"
