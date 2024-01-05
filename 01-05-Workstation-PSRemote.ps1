43db7fa7c990d1ce2cd5e1869bdd6f4f41845256
Tor Ivar Melling <tor.i.melling@ntnu.no>
2024-01-05 13:39:21 +0100

    update

--- 01-05-Workstation-PSRemote.ps1
+++ 01-05-Workstation-PSRemote.ps1
@@ -1,3 +1,8 @@
+# TEST PSREMOTE: Connect-PSRemoting -ComputerName FROM MGR TO WORKSTATIONS/SERVERS IN DOMAIN
+Enter-PSSession -ComputerName dc1
+Enter-PSSession -ComputerName srv1
+Enter-PSSession -ComputerName cl1
+
 # THIS SCRIPT MUST BE RUN AS ADMINISTRATOR ON VM'S WITH WINDOWS 10/11 IF PSREMOTE DONT WORK
 winrm set winrm/config/service/auth '@{Kerberos="true"}' 
 # List auth: 
