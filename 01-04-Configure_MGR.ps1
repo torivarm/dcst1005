0d23cd35d9f3c8a5694547cafac66b58817764d5
Tor Ivar Melling <tor.i.melling@ntnu.no>
2024-01-12 09:50:55 +0100

    update

--- 01-04-Configure_MGR.ps1
+++ 01-04-Configure_MGR.ps1
@@ -13,7 +13,7 @@ git config --global user.name "NAVN"
 git config --global user.email "EPOST@EPOST.EPOST"
 #
 
-# Check if computer is domain joined 
+# Check if computer is domain joined (CIM (Common Information Model))
 Get-CimInstance -ClassName Win32_ComputerSystem | Select-Object -Property Name, Domain, PartOfDomain
 
 # Remote Server Administrative Tools (RSAT)
