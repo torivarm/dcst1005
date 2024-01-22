e006e62e589cb86633c37bc04442fcade36a18bd
Tor Ivar Melling <tor.i.melling@ntnu.no>
2024-01-22 15:26:21 +0100

    update

--- 03-03-AddUsersToGroupBasedOnDepartment.ps1
+++ 03-03-AddUsersToGroupBasedOnDepartment.ps1
@@ -1,7 +1,8 @@
 
 $departments = @("hr","sales","consultant","it","finance")
 foreach ($department in $departments) {
-    $departmentUsers = Get-ADUser -Filter "Department -eq '$department'" -Properties Department | Select-Object samAccountName, Name, Department
+    $departmentUsers = Get-ADUser -Filter "Department -eq '$department'" -Properties Department | 
+                        Select-Object samAccountName, Name, Department
     
     foreach ($user in $departmentUsers) {
         Add-ADGroupMember -Identity "g_$department" -Members $user.samAccountName
