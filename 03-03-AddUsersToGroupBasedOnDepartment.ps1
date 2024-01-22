6464f909a32e568470f99c667358970d234bf707
Tor Ivar Melling <tor.i.melling@ntnu.no>
2024-01-22 15:25:45 +0100

    Update

---
+++ 03-03-AddUsersToGroupBasedOnDepartment.ps1
@@ -0,0 +1,12 @@
+
+$departments = @("hr","sales","consultant","it","finance")
+foreach ($department in $departments) {
+    $departmentUsers = Get-ADUser -Filter "Department -eq '$department'" -Properties Department | Select-Object samAccountName, Name, Department
+    
+    foreach ($user in $departmentUsers) {
+        Add-ADGroupMember -Identity "g_$department" -Members $user.samAccountName
+    }
+
+
+}
+
