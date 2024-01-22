bc74405a634a2484ccc5b1f5c426524e4a704ea2
Tor Ivar Melling <tor.i.melling@ntnu.no>
2024-01-22 10:20:47 +0100

    Update

--- 03-01-Create-ADGroup-wUsers.ps1
+++ 03-01-Create-ADGroup-wUsers.ps1
@@ -4,3 +4,10 @@ $rootFolder = "C:\git-projects\dcst1005\dcst1005\"
 # CSV-file with users
 $groups = Import-Csv -Path "$rootFolder\tmp_csv-users-example.csv" -Delimiter ","
 
+# Initialize arrays to store the results
+$groupsCreated = @()
+$groupsNotCreated = @()
+
+foreach ($group in $groups) {
+    $existingGroup = Get-ADGroup -Filter "Name -eq '$group)'" -ErrorAction SilentlyContinue
+}
