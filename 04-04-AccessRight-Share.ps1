3ee6e6a269c14cfef4ffedab77d2fe6af77cf4e2
Tor Ivar Melling <tor.i.melling@ntnu.no>
2024-01-29 11:55:20 +0100

    Update

--- 04-04-AccessRight-Share.ps1
+++ 04-04-AccessRight-Share.ps1
@@ -1,19 +1,19 @@
-# Change ACL for HR Share (make sure do the same for all shares)
+# Change ACL for IT Share (make sure do the same for all shares)
 
-$acl = Get-acl -Path "\\infrait.sec\files\HR-Share"
+$acl = Get-acl -Path "\\infrait.sec\files\IT-Share"
 $acl.access
-$AccessRule = New-Object System.Security.AccessControl.FileSystemAccessRule("l_FileShareHR_FullControll","FullControl","Allow")
+$AccessRule = New-Object System.Security.AccessControl.FileSystemAccessRule("l_FileShareIT_FullControll","FullControl","Allow")
 $acl.SetAccessRule($AccessRule)
-$acl | Set-Acl -Path "\\infrait.sec\files\HR-Share"
+$acl | Set-Acl -Path "\\infrait.sec\files\IT-Share"
 
-$acl = Get-acl -Path "\\infrait.sec\files\HR-Share"
+$acl = Get-acl -Path "\\infrait.sec\files\IT-Share"
 $acl.access
 
 # Change inheritance for HR Share (make sure do the same for all shares)
 
-$ACL = Get-Acl -Path "\\infrait.sec\files\HR-Share"
+$ACL = Get-Acl -Path "\\infrait.sec\files\IT-Share"
 $ACL.SetAccessRuleProtection($true,$true)
-$ACL | Set-Acl -Path "\\infrait.sec\files\HR-Share"
+$ACL | Set-Acl -Path "\\infrait.sec\files\IT-Share"
 
 <#
 $true: Enable ACL protection, which means the folder will no longer inherit permissions from its parent.
@@ -21,10 +21,10 @@ $true: Preserve inherited permissions by converting them into explicit permissio
 #>
 
 # Remove BUILTIN\Users from ACL for HR Share (make sure do the same for all shares)
-$acl = Get-Acl "\\infrait.sec\files\HR-Share"
+$acl = Get-Acl "\\infrait.sec\files\IT-Share"
     $acl.Access | Where-Object {$_.IdentityReference -eq "BUILTIN\Users" } | ForEach-Object { $acl.RemoveAccessRuleSpecific($_) }
-    Set-Acl "\\infrait.sec\files\HR-Share" $acl
-    (Get-ACL -Path "\\infrait.sec\files\HR-Share").Access | 
+    Set-Acl "\\infrait.sec\files\IT-Share" $acl
+    (Get-ACL -Path "\\infrait.sec\files\IT-Share").Access | 
         Format-Table IdentityReference,FileSystemRights,AccessControlType,IsInherited,InheritanceFlags -AutoSize
 
 
