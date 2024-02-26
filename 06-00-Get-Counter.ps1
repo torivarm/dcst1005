53d6db7a639e9e840d61f03a3471f194baa9c396
Tor Ivar Melling <tor.i.melling@ntnu.no>
2024-02-26 09:52:15 +0100

    Update

---
+++ 06-00-Get-Counter.ps1
@@ -0,0 +1,9 @@
+Get-Counter -ListSet *
+Get-Counter -ListSet * | Select-Object -ExpandProperty CounterSetName
+
+Get-Counter -ListSet * | Where-Object {$_.CounterSetName -like "*Processor*"} | Select-Object -ExpandProperty CounterSetName
+Get-Counter -ListSet * | Where-Object {$_.CounterSetName -like "*Memory*"} | Select-Object -ExpandProperty CounterSetName
+
+# Gets all the counters for the Processor object
+Get-Counter -ListSet Processor | Select-Object -ExpandProperty Counter
+Get-Counter -Counter "\Processor(*)\% Processor Time" -SampleInterval 2 -MaxSamples 10
