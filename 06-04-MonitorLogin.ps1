6a2dde42e8a8859b0dcf1d3d03286eaa3203fccc
Tor Ivar Melling <tor.i.melling@ntnu.no>
2024-02-28 14:59:31 +0100

    update

--- 06-04-MonitorLogin.ps1
+++ 06-04-MonitorLogin.ps1
@@ -2,28 +2,27 @@
 # Set-NetFirewallRule -Name "RemoteEventLogSvc-In-TCP" -Enabled True
 
 
-# Define the remote domain controller's name
-$DC1 = "DC1"
-
-# Define the number of recent events to retrieve
-$NumberOfEvents = 10
-
-# Define event IDs for successful and failed logon attempts
-$EventIDs = 4624, 4625
-
-# Retrieve and display the specified number of recent logon events
-foreach ($EventID in $EventIDs) {
-    Write-Host "Retrieving the last $NumberOfEvents events for Event ID: $EventID" -ForegroundColor Cyan
-    Get-WinEvent -ComputerName $DC1 -FilterHashtable @{LogName='Security'; ID=$EventID} -MaxEvents $NumberOfEvents | ForEach-Object {
-        $Event = [PSCustomObject]@{
+$ClientMachines = "Cl1", "DC1", "SRV1", "mgr" # Add your client machine names here
+$ScriptBlock = {
+    # This is the same script block you'd use for a single machine, focusing on Event ID 4625
+    Get-WinEvent -FilterHashtable @{
+        LogName='Security';
+        ID=4625;
+    } -MaxEvents 10 | ForEach-Object {
+        [PSCustomObject]@{
             TimeCreated = $_.TimeCreated
             EventID = $_.Id
             UserName = $_.Properties[5].Value
             LogonType = $_.Properties[8].Value
-            IPAddress = $_.Properties[18].Value
+            IPAddress = $_.Properties[19].Value
             LogonProcess = $_.Properties[11].Value
         }
-        Write-Output $Event
     }
-    Write-Host "-------------------------------------------------------------" -ForegroundColor Green
 }
+
+foreach ($Client in $ClientMachines) {
+    Write-Host "Querying $Client for non-successful logon events..."
+    Invoke-Command -ComputerName $Client -ScriptBlock $ScriptBlock -ErrorAction SilentlyContinue | Format-Table -AutoSize
+    Write-Host "-------------------------------------------------------------"
+}
+
