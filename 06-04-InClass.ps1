76b89f71259e1abbdb3a1af073ab1fccf69356bd
Tor Ivar Melling <tor.i.melling@ntnu.no>
2024-03-01 12:49:44 +0100

    test

--- 06-04-InClass.ps1
+++ 06-04-InClass.ps1
@@ -5,5 +5,5 @@ Get-WinEvent -ListLog *
 Get-WinEvent -LogName Application
 Get-WinEvent -LogName Security
 
-Get-WinEvent -FilterHashtable @{LogName='Security'; ID=}
+Get-WinEvent -FilterHashtable @{LogName='Security'; ID=4625}
 
