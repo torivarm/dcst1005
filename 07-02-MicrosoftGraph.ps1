f61f57e043e0796b3ad4f49a0204467f861bce48
Tor Ivar Melling <tor.i.melling@ntnu.no>
2024-03-11 13:12:42 +0100

    Update

--- 07-02-MicrosoftGraph.ps1
+++ 07-02-MicrosoftGraph.ps1
@@ -1,2 +1,19 @@
 $TenantID = "bd0944c8-c04e-466a-9729-d7086d13a653"
 Connect-MgGraph -TenantId $TenantID -Scopes "User.ReadWrite.All", "Group.ReadWrite.All", "Directory.ReadWrite.All", "RoleManagement.ReadWrite.Directory"
+
+
+$Details = Get-MgContext
+$Scopes = $Details | Select-Object -ExpandProperty Scopes
+$Scopes = $Scopes -join ","
+$OrgName = (Get-MgOrganization).DisplayName
+""
+""
+"Microsoft Graph current session details:"
+"---------------------------------------"
+"Tenant Id = $($Details.TenantId)"
+"Client Id = $($Details.ClientId)"
+"Org name  = $OrgName"
+"App Name  = $($Details.AppName)"
+"Account   = $($Details.Account)"
+"Scopes    = $Scopes"
+"---------------------------------------"
