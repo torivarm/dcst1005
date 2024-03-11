ea72bb5462bf60757596eac98d3e1f0ae74e2606
Tor Ivar Melling <tor.i.melling@ntnu.no>
2024-03-11 21:53:18 +0100

    update

--- 07-02-MicrosoftGraph.ps1
+++ 07-02-MicrosoftGraph.ps1
@@ -1,3 +1,8 @@
+# Install Microsoft Graph Module
+# Install-Module Microsoft.Graph
+# Get-InstaledModule -Name Microsoft.Graph.*
+
+
 $TenantID = "bd0944c8-c04e-466a-9729-d7086d13a653"
 Connect-MgGraph -TenantId $TenantID -Scopes "User.ReadWrite.All", "Group.ReadWrite.All", "Directory.ReadWrite.All", "RoleManagement.ReadWrite.Directory"
 
---
+++ 07-03-ImportUsersEntraID.ps1
@@ -0,0 +1,25 @@
+$TenantID = "bd0944c8-c04e-466a-9729-d7086d13a653"
+Connect-MgGraph -TenantId $TenantID -Scopes "User.ReadWrite.All", "Group.ReadWrite.All", "Directory.ReadWrite.All", "RoleManagement.ReadWrite.Directory"
+
+$users = Import-CSV -Path '/Users/melling/git-projects/dcst1005/07-00-CSV-Users.csv' -Delimiter ","
+
+$PasswordProfile = @{
+    Password = 'DemoPassword12345!'
+    }
+foreach ($user in $users) {
+    $Params = @{
+        UserPrincipalName = $user.userPrincipalName + "@digsec.onmicrosoft.com"
+        DisplayName = $user.displayName
+        GivenName = $user.GivenName
+        Surname = $user.Surname
+        MailNickname = $user.userPrincipalName
+        AccountEnabled = $true
+        PasswordProfile = $PasswordProfile
+        Department = $user.Department
+        CompanyName = "InfraIT Sec"
+        Country = "Norway"
+        City = "Trondheim"
+    }
+    $Params
+    New-MgUser @Params
+}
