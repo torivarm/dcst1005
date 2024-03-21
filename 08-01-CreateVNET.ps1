4626d500a7bda602edf6bc945e6369b702872bae
Tor Ivar Melling <tor.i.melling@ntnu.no>
2024-03-21 11:18:36 +0100

    Update

--- 08-01-CreateVNET.ps1
+++ 08-01-CreateVNET.ps1
@@ -5,10 +5,14 @@
 # Import the Az module
 # Import-Module Az
 
+
+
 # Variables
 $tenantID = "bd0944c8-c04e-466a-9729-d7086d13a653" # Remember to change this to your own TenantID
 $subscrptionID = "41082359-57d6-4427-b5d9-21e269157652" # Remember to change this to your own SubscriptionID
 
 # Connect to Azure
 Connect-AzAccount -Tenant $tenantID -Subscription $subscrptionID
+
+
 
