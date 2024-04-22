6f0179e64b68d0f8b31aa418dcfe3f14c8b90456
Tor Ivar Melling <tor.i.melling@ntnu.no>
2024-04-22 14:24:44 +0200

    update

--- Weekly assignment script for delivery/WA12-ListConfigForHubSpokeAndn-Tier.ps1
+++ Weekly assignment script for delivery/WA12-ListConfigForHubSpokeAndn-Tier.ps1
@@ -4,4 +4,14 @@ $resourceGroupName = $prefix + '-rg-network-001'
 $resourceGroupNameVM = $prefix + '-rg-vm-001'
 $location = 'uksouth'
 
-# Get All VNETs and Subnets that starts with the $prefix
+# Gets all resources in two resources groups based on its $prefix
+$resources = Get-AzResource -ResourceGroupName $resourceGroupName
+$resourcesVM = Get-AzResource -ResourceGroupName $resourceGroupNameVM
+
+# List all resources in the network resource group
+# Gets the name of all VNETs, its subnets and the address prefix
+# Gets the name of all NSGs and its rules and which subnets they are associated with
+# Gets the name of all ASGs and associated nics
+
+
+
