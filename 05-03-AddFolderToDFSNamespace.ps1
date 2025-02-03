59ae237d6b545560350c9eba6ec241b8d5d5a0f1
Tor Ivar Melling <tor.i.melling@ntnu.no>
2025-02-03 11:35:08 +0100

    update

--- 05-03-AddFolderToDFSNamespace.ps1
+++ 05-03-AddFolderToDFSNamespace.ps1
@@ -1,1 +1,33 @@
- 
+# In this scrip we will add the created shared folder for installfiles to the dfsnamespace infrait\files on SRV1
+
+# Define the remote computer name
+$remoteComputerName = "srv1"
+
+# Define the share name
+$shareName = "InstallFiles"
+
+# Define the folderName
+$folderName = "installfiles"
+
+# Define the ShareFolderPath
+$ShareFolderPath = "C:\shares\$folderName"
+
+# Define the DFSNamespace
+$DFSNamespace = "InfraIT\Files"
+
+$dfsfolder = "\\$remoteComputerName\$shareName"
+
+# DFS Target Path
+$DFSTargetPath = "\\$remoteComputerName\$shareName"
+
+# Create a new PowerShell session to the remote server
+$session = New-PSSession -ComputerName $remoteComputerName
+
+# Add the folder to the DFS Namespace and add new dfsn folder target
+Invoke-Command -Session $session -ScriptBlock {
+    param($DFSNamespace, $folderName, $DFSTargetPath)
+    # Add the folder to the DFS Namespace
+    New-DfsnFolder -Path "$DFSNamespace\$folderName" -TargetPath $DFSTargetPath
+    New-DfsnFoldertarget -Path "$DFSNamespace\$folderName" -TargetPath $DFSTargetPath
+    Write-Host "Folder added to DFS Namespace"
+} -ArgumentList $DFSNamespace, $folderName, $DFSTargetPath
